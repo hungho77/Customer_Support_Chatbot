15af0fdac7bbe985263c37a376b85af1c06fbf2c
hungho77 <hothinhhung77@gmail.com>
2025-03-09 22:58:20 +0700

    update vector database

--- src/vector_store.py
+++ src/vector_store.py
@@ -23,12 +23,12 @@ def build_vector_store(collection_name='MyCollection'):
             df = pd.read_csv(file_path)
 
             # Clean the 'Answer' column from special characters
-            df['Answer'] = df['Answer'].str.replace('\u200b', '', regex=False)
-            df['Answer'] = df['Answer'].str.replace('\xa0', '', regex=False)
-            df['Answer'] = df['Answer'].str.replace('"', '', regex=False)
+            df['summary'] = df['summary'].str.replace('\u200b', '', regex=False)
+            df['summary'] = df['summary'].str.replace('\xa0', '', regex=False)
+            df['summary'] = df['summary'].str.replace('"', '', regex=False)
 
             # Append answers to the list
-            answers.extend(df['Answer'].tolist())
+            answers.extend(df['summary'].tolist())
 
     # Initialize the Qdrant client
     client = QdrantClient(host=QDRANT_HOST, port=QDRANT_PORT)
